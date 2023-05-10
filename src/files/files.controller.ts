9a446f88836df6af9e00f330fb3f49df85c8c106
JaacielBriseo <jaacielbris23@gmail.com>
2023-05-09 19:49:55 -0700

    Serve images static

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -9,14 +9,18 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { FilesService } from './files.service';
-import { diskStorage } from 'multer';
-import { fileFilter, fileNamer } from './helpers';
+import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
+import { diskStorage } from 'multer';
+import { FilesService } from './files.service';
+import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -43,7 +47,9 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
-    const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${
+      file.filename
+    }`;
 
     return {
       secureUrl,
